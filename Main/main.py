a983e8a6990192231d49a16446bf0e7b6465b23e
guvzan <vernmaks@gmail.com>
2022-12-07 12:37:52 +0200

    Added Ship + Settings

--- Main/main.py
+++ Main/main.py
@@ -1,23 +1,45 @@
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Основний клас"""
     def __init__(self):
         """Ініціалізація"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """Головний цикл"""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
+            self._check_events()
+            self._update_screen()
 
-            pygame.display.flip()
 
+
+
+    def _check_events(self):
+        """Диспетчер подій"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 10
+
+    def _update_screen(self):
+        """Оновлення екрану"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == "__main__":
     ai = AlienInvasion()
