e057361186415641c3a5b37c0bad23c7aa544fd8
guvzan <vernmaks@gmail.com>
2022-12-11 12:23:52 +0200

    Updated Shooting

--- Main/alien.py
+++ Main/alien.py
@@ -5,11 +5,12 @@ class Alien(Sprite):
     """Клас прибульця"""
     def __init__(self, ai_game):
         """Ініціалізація"""
-        super.__init__()
+        super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         #Загрузити картинку
-        self.image = pygame.image.load('../images/ship.bmp')
+        self.image = pygame.image.load('../images/alien.bmp')
         self.rect = self.image.get_rect()
 
         #Спавнити справа зверху
@@ -18,3 +19,16 @@ class Alien(Sprite):
 
         #Зберігати поточну позицію
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Змістити прибульця праворуч чи ліворуч"""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """Повертає істину, якщо прибулець на краю екрана"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >=screen_rect.right or self.rect.left<=0:
+            return True
+
+
--- Main/main.py
+++ Main/main.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Основний клас"""
@@ -18,6 +19,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
 
     def run_game(self):
@@ -25,11 +29,9 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
+            self._update_aliens()
             self._update_screen()
-
-
-
 
     def _check_events(self):
         """Диспетчер подій"""
@@ -41,7 +43,6 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-
 
     def _check_keydown_events(self, event):
         """Реагувати на натискання клавіщ"""
@@ -73,13 +74,72 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
     def _update_bullets(self):
-        """Оновити позицію куль та позьутись старих куль"""
+        """Оновити позицію куль та позбутись старих куль"""
+        self.bullets.update()
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+
+
+    def _check_bullet_alien_collisions(self):
+        """Реакція на зіткнення куль і прибульців"""
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+
+    def _create_fleet(self):
+        """Створити флот прибульців"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien_width = alien.rect.width
+        avaliable_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = avaliable_space_x // (2 * alien_width)
+
+        ship_height = self.ship.rect.height
+        avaliable_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = avaliable_space_y // (2 * alien_height)
+
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Створити і заспавнити прибульця"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
+    def _update_aliens(self):
+        """Оновити позиції всіх прибульців"""
+        self._check_fleet_edges()
+        self.aliens.update()
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!1")
+
+    def _check_fleet_edges(self):
+        """Перевірка, чи прибулець досяг краю"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Спуск флоту та зміна напрямку"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
 
 
 if __name__ == "__main__":
